20d84326933d60268765c1820fe9b551d29f2883
eboualla <eboualla@student.42vienna.com>
2026-05-08 17:31:14 +0200

    base architecture for ftprintf

--- ft_printf.c
+++ ft_printf.c
@@ -6,36 +6,56 @@
 /*   By: eboualla <eboualla@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 19:25:37 by eboualla          #+#    #+#             */
-/*   Updated: 2026/05/08 15:29:39 by eboualla         ###   ########.fr       */
+/*   Updated: 2026/05/08 17:30:38 by eboualla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "ft_printf.h"
 
-int ft_printf(const char *n, )
+// checks type of descriptor and handles it
+void	check_conv(const char *str, va_list args, int i)
 {
-	va_list args;
-	va_start (args, n);
-	va_arg ;
-	va_end ;
+	if (str[i + 1] == d)
+		handle_int(va_arg(args, int));
+	else if (str[i] == c)
+		handle_char(va_arg(args, char);
+}
 
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+	size_t	i;
+	int		count;
 
-	
-
-	while (ft_isprint())
+	i = 0;
+	count = 0;
+	va_start(args, str);
+	while (ft_isprint(str[i]))
 	{
-		if (is_char())
-			ft_putchar();
-
+		if (str[i] != '%')
+			write(1, &str[i], 1);
+		else
+		{
+			if (str[i + 1] == '%')
+				write(1, '%', 1);
+			else
+				check_conv(str, args, i);
+		}
+		i++;
 	}
+	return (count);
 }
 
 #include <stdio.h>
 
-int main (void)
+int	main(void)
 {
-	int i = 5;
-	char c = 'e';
-	char *s = "Hello";
+	int		i;
+	char	c;
+	char	*s;
 
-	ft_printf(i);
-	printf(i);
+	i = 5;
+	c = 'e';
+	s = "Hello";
+	ft_printf("Hello%d or %c", i, c);
+	printf("Hello%d or %c", i, c);
 }
