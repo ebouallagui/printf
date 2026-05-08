a52758423dc15cfabac3697a9da00758be85c8a4
eboualla <eboualla@student.42vienna.com>
2026-05-08 18:43:04 +0200

    some additions in makefiel & headerfile

--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,18 @@
 /*   By: eboualla <eboualla@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 19:27:39 by eboualla          #+#    #+#             */
-/*   Updated: 2026/05/03 19:29:22 by eboualla         ###   ########.fr       */
+/*   Updated: 2026/05/08 18:42:07 by eboualla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF
 # define FT_PRINTF
 
-# include
+# include <stdarg.h>
+# include "libft.h"
 
-int	ft_printf(const char *, )
+int	ft_printf(const char *str, ...);
+
+int handle_char(char c);
+
+#endif
